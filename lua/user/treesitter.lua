f473f2835d61281c618093eb1f9084d902d193b0
Hazhir Derakhshi <hajear@gmail.com>
2022-04-26 23:26:37 +0100

    Add autopairs plugin

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,22 +1,15 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-local configs = require("nvim-treesitter.configs")
 configs.setup {
   ensure_installed = "maintained",
-  sync_install = false, 
+  sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
+  autopairs = {
+    enable = true,
+  },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
