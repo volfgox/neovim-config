36b286e638b632f0d9d8a1f0e861c5e929be84c1
Hazhir Derakhshi <hajear@gmail.com>
2022-04-27 17:06:21 +0100

    Change colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,8 +54,8 @@ return packer.startup(function(use)
   use "akinsho/toggleterm.nvim"
 
   -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  -- use "lunarvim/darkplus.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
